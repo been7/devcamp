968b12ae8e3b02c1c2d8a9ad0e5a6c1a9c568d3e
Been7 <qlsl707@hanmail.net>
2024-03-06 20:50:16 +0900

    240306 회원가입 페이지

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,8 +7,15 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import {
   Select,
   SelectContent,
@@ -16,9 +23,10 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { useToast } from "@/components/ui/use-toast";
 import { registerSchema } from "@/validators/auth";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { ArrowRight } from "lucide-react";
+import { ArrowLeft, ArrowRight } from "lucide-react";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
@@ -27,6 +35,7 @@ type RegisterInput = z.infer<typeof registerSchema>;
 
 export default function Join() {
   const [step, setStep] = useState<number>(0);
+  const { toast } = useToast();
 
   const joinForm = useForm<RegisterInput>({
     resolver: zodResolver(registerSchema),
@@ -35,54 +44,174 @@ export default function Join() {
       email: "",
       phone: "",
       role: "",
+      password: "",
+      confirmPassword: "",
     },
   });
 
+  const handleNextButton = () => {
+    joinForm.trigger(["phone", "email", "username", "role"]);
+    const phoneState = joinForm.getFieldState("phone");
+    const emailState = joinForm.getFieldState("email");
+    const usernameState = joinForm.getFieldState("username");
+    const roleState = joinForm.getFieldState("role");
+
+    if (!phoneState.isDirty || phoneState.invalid) return;
+    if (!emailState.isDirty || emailState.invalid) return;
+    if (!usernameState.isDirty || usernameState.invalid) return;
+    if (!roleState.isDirty || roleState.invalid) return;
+
+    setStep(1);
+  };
+
+  const handleJoinSubmit = (data: RegisterInput) => {
+    if (data.password !== data.confirmPassword) {
+      toast({
+        variant: "destructive",
+        title: "비밀번호가 일치하지 않습니다.",
+        duration: 1000,
+      });
+
+      return;
+    }
+
+    alert(JSON.stringify(data, null, 4));
+  };
+
   return (
-    <Card className="w-[350px] m-auto ">
-      <CardHeader>
-        <CardTitle>계정을 생성합니다.</CardTitle>
-        <CardDescription>필수 정보를 입력해볼게요.</CardDescription>
-      </CardHeader>
-      <CardContent>
-        <form>
-          <div className="grid w-full items-center gap-4">
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="name">이름</Label>
-              <Input id="name" placeholder="홍길동" />
-            </div>
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="name">이메일</Label>
-              <Input id="name" placeholder="hello@sparta-devcamp.com" />
-            </div>
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="name">연락처</Label>
-              <Input id="name" placeholder="01000000000" />
-            </div>
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="framework">역할</Label>
-              <Select>
-                <SelectTrigger id="framework">
-                  <SelectValue placeholder="역할을 선택해주세요." />
-                </SelectTrigger>
-                <SelectContent position="popper">
-                  <SelectItem value="next">관리자</SelectItem>
-                  <SelectItem value="sveltekit">일반사용자</SelectItem>
-                </SelectContent>
-              </Select>
-            </div>
+    <div className="flex justify-center items-center h-screen">
+      <Card className="w-[350px]">
+        <CardHeader>
+          <CardTitle>계정을 생성합니다.</CardTitle>
+          <CardDescription>필수 정보를 입력해볼게요.</CardDescription>
+        </CardHeader>
+        {step === 0 && (
+          <div>
+            <CardContent>
+              <Form {...joinForm}>
+                <FormField
+                  control={joinForm.control}
+                  name="username"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>이름</FormLabel>
+                      <FormControl>
+                        <Input placeholder="홍길동" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={joinForm.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>이메일</FormLabel>
+                      <FormControl>
+                        <Input placeholder="abc123@test.com" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={joinForm.control}
+                  name="phone"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>연락처</FormLabel>
+                      <FormControl>
+                        <Input placeholder="01012345678" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={joinForm.control}
+                  name="role"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>역할</FormLabel>
+                      <Select
+                        onValueChange={field.onChange}
+                        defaultValue={field.value}
+                      >
+                        <FormControl>
+                          <SelectTrigger>
+                            <SelectValue placeholder="역할을 선택하세요." />
+                          </SelectTrigger>
+                        </FormControl>
+                        <SelectContent>
+                          <SelectItem value="admin">관리자</SelectItem>
+                          <SelectItem value="user">사용자</SelectItem>
+                        </SelectContent>
+                      </Select>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </Form>
+            </CardContent>
+            <CardFooter className="flex justify-between">
+              <Button type="button" className="join" onClick={handleNextButton}>
+                다음 단계로 <ArrowRight className="w-4 h-4 ml-2" />
+              </Button>
+            </CardFooter>
           </div>
-        </form>
-      </CardContent>
-      <CardFooter className="flex justify-between">
-        <Button
-          type="button"
-          className={step === 0 ? "join" : "hidden"}
-          onClick={() => setStep(1)}
-        >
-          다음 단계로 <ArrowRight className="w-4 h-4 ml-2" />
-        </Button>
-      </CardFooter>
-    </Card>
+        )}
+        {step === 1 && (
+          <div>
+            <CardContent>
+              <Form {...joinForm}>
+                <form onSubmit={joinForm.handleSubmit(handleJoinSubmit)}>
+                  <FormField
+                    control={joinForm.control}
+                    name="password"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>비밀번호</FormLabel>
+                        <FormControl>
+                          <Input type="password" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={joinForm.control}
+                    name="confirmPassword"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>비밀번호 확인</FormLabel>
+                        <FormControl>
+                          <Input type="password" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </form>
+              </Form>
+            </CardContent>
+            <CardFooter className="flex justify-between">
+              <Button variant="outline" onClick={() => setStep(0)}>
+                <ArrowLeft className="mr-2" />
+                이전 단계로
+              </Button>
+              {/* <Button type="submit" className=""> */}
+              <Button
+                type="button"
+                className=""
+                onClick={joinForm.handleSubmit(handleJoinSubmit)}
+              >
+                가입 완료
+              </Button>
+            </CardFooter>
+          </div>
+        )}
+      </Card>
+    </div>
   );
 }
